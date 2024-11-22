3023d57bf87b1ba82a81ed3a512136dda969b34d
Lego4A <92343526+Lego4A@users.noreply.github.com>
2024-11-21 22:10:18 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBWHJbM7cTJ0oJGs686k4pEWvcJCnZ3g8k",
+    authDomain: "sarah-promposal-212a.firebaseapp.com",
+    projectId: "sarah-promposal-212a",
+    storageBucket: "sarah-promposal-212a.appspot.com",
+    messagingSenderId: "810538275538",
+    appId: "1:810538275538:web:331b46c73bcf0f1e39d30b",
+    measurementId: "G-7YHLVDF40Q"
+};
+
+export default firebaseConfig;
